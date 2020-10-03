4e536620e0202cf3be01ba8f178e861590259d12
Lilya Zilyukina <zilyukina.liliya@gmail.com>
2020-10-03 19:15:23 +0300

    base routing added (#4)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main";
-
+import SignIn from "../sign-in/sign-in";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 const App = (props) => {
   const {placesAmount} = props;
   return (
-    <Main placesAmount={placesAmount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesAmount={placesAmount} />
+        </Route>
+      </Switch>
+      <Route path="/login" exact component={SignIn}></Route>
+      <Route path="/favorites" exact component={Favorites}></Route>
+      <Route path="/offer/:id?" exact component={Room}></Route>
+    </BrowserRouter>
   );
 };
 
