75700549e16e9c32f3934f5706a29976918ed4df
Liliya <zilyukina.liliya@gmail.com>
2020-10-13 14:00:26 +0300

    dates const fixed

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,18 +1,18 @@
 export const getFormattedDate = (dateStr) => {
-  const MONTHS_NAMES = {
-    0: `January`,
-    1: `February`,
-    2: `March`,
-    3: `April`,
-    4: `May`,
-    5: `June`,
-    6: `July`,
-    7: `August`,
-    8: `September`,
-    9: `October`,
-    10: `November`,
-    11: `December`
-  };
+  const MONTHS_NAMES = [
+    `January`,
+    `February`,
+    `March`,
+    `April`,
+    `May`,
+    `June`,
+    `July`,
+    `August`,
+    `September`,
+    `October`,
+    `November`,
+    `December`
+  ];
   const date = new Date(dateStr);
   const year = date.getFullYear();
   const month = date.getMonth();
