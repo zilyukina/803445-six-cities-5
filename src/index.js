c39ca0c1cc401e6f91015e04f511165ca821d6b3
Liliya <zilyukina.liliya@gmail.com>
2020-10-24 15:24:16 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import offers from "./mocks/offers";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./store/reducer";
 
 const SETTINGS = {
   PLACES_AMOUNT: 312
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      placesAmount={SETTINGS.PLACES_AMOUNT}
-      offers={offers}/>,
+    <Provider store={store}>
+      <App
+        placesAmount={SETTINGS.PLACES_AMOUNT}
+        offers={offers}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
